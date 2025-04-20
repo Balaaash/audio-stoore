2ef93a7c219d68cc729f80baca9cb305990a8b41
Balaaash <bingersoft@gmail.com>
2025-04-20 20:11:02 +0300

    add build

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Catalog from './pages/Catalog';
+import Cart from './pages/Cart';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Cart from './pages/Cart';
 
 const App = () => {
   return (
     <Router>
-      <Header />
-      <main className="p-4 min-h-[calc(100vh-128px)] mx-auto max-w-5xl">
-      <Routes>
-        <Route path="/" element={<Catalog />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
-      </main>
-      <Footer />
+      <CartProvider>
+        <Header />
+
+        {/* вот здесь контейнер — он ограничит по ширине и отцентрирует */}
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Catalog />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </div>
+
+        <Footer />
+      </CartProvider>
     </Router>
   );
 };
